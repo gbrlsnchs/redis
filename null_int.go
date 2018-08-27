0e414e353d754d43083dd91111d47229e234f8bc
Gabriel Sanches <gsanches.dev@gmail.com>
2018-08-27 20:16:29 -0300

    Remove null types

--- null_int.go
+++
@@ -1,7 +0,0 @@
-package redis
-
-// NullInt64 represents an int64 that may be null.
-type NullInt64 struct {
-	Int64 int64
-	Valid bool
-}
--- null_string.go
+++
@@ -1,7 +0,0 @@
-package redis
-
-// NullString represents a string that may be null.
-type NullString struct {
-	String string
-	Valid  bool
-}
