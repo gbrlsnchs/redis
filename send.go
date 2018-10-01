990b387520e8849b44c2edfed5757395fa5d1295
Gabriel Sanches <g@lonemaker.com>
2018-10-01 02:09:28 -0300

    Read connection completely before closing it

--- send.go
+++ send.go
@@ -9,29 +9,32 @@ import (
 
 func send(ctx context.Context, w *internal.Writer, conn net.Conn, times int, cmd string, args ...interface{}) (*Result, error) {
 	var err error
-	rc := make(chan *Result, 1)
-	ec := make(chan error, 1)
-	go func() {
-		conn := conn.(*internal.Conn)
-		defer conn.Unlock()
-		if _, err = w.WriteCmd(cmd, args...); err != nil {
-			ec <- err
-			return
-		}
-		var r *Result
-		if r, err = read(conn, times); err != nil {
-			ec <- err
-			return
-		}
+	rc := make(chan *Result)
+	ec := make(chan error)
 
-		rc <- r
-	}()
+	// Check if context is done, otherwise wait for the whole connection to be read.
 	select {
 	case <-ctx.Done():
 		return nil, ctx.Err()
-	case err = <-ec:
-		return nil, err
-	case r := <-rc:
-		return r, nil
+	default:
+		go func() {
+			if _, err = w.WriteCmd(cmd, args...); err != nil {
+				ec <- err
+				return
+			}
+			var r *Result
+			if r, err = read(conn, times); err != nil {
+				ec <- err
+				return
+			}
+
+			rc <- r
+		}()
+		select {
+		case err = <-ec:
+			return nil, err
+		case r := <-rc:
+			return r, nil
+		}
 	}
 }
